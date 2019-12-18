095213d1c288db7502767c2a4a6523ed41e475f2
Arulraj A <arul.raj47@gmail.com>
2019-12-18 16:58:35 +0530

    Added the project files.

---
+++ README.md
@@ -0,0 +1,40 @@
+# How to Group and Filter the Vue MultiSelect Dropdown Items of Syncfusion
+
+Get a quick overview on how to group and filter items in Vue MultiSelect Dropdown. You will learn how to group the Vue MultiSelect Dropdown items as well as how to filter those items. You will also see different filter types in MultiSelect Dropdown, how to apply those filters to items, and how to include diacritic characters.
+
+Documentation Links: 
+https://ej2.syncfusion.com/vue/documentation/multi-select/grouping
+https://ej2.syncfusion.com/vue/documentation/multi-select/filtering
+
+
+Demo Links: 
+https://ej2.syncfusion.com/vue/demos/#/material/multi-select/grouping-icon.html
+https://ej2.syncfusion.com/vue/demos/#/material/multi-select/filtering.html
+
+
+## Project pre-requisites
+Make sure that you have the compatible versions of Node and Vue-Cli in your machine before starting to work on this project.
+
+## How to run this application?
+To run this application, you need to first clone the `how-to-group-and-filter-the-vue-multiselect-dropdown-items` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionExamples/how-to-group-and-filter-the-vue-multiselect-dropdown-items multiselectdropdown-component
+cd multiselectdropdown-component
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed Vue packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running on development server
+Run `npm run serve` command for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
+## Further help
+
+To get more help on the vue CLI use go check out the [Vue-Cli README](https://github.com/vuejs/vue-cli/blob/master/README.md).
---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      <div style="margin:10% 40%; width:250px;">
+        <ejs-multiselect popupHeight="200px" popupWidth="250px" 
+        :dataSource='dataItem' :fields='dataFields' 
+        placeholder='Select a vegetable' :allowFiltering='true'
+        :ignoreCase='false' filterType='Contains' enableGroupCheckBox='true'
+        mode='CheckBox'>
+        </ejs-multiselect>
+        <!-- <ejs-multiselect popupHeight="200px" popupWidth="250px" 
+        :dataSource='dataItem' :fields='dataFields' 
+        placeholder='Select a value' :allowFiltering='true'
+        :ignoreAccent='true' enableGroupCheckBox='true'
+        mode='CheckBox'>
+        </ejs-multiselect> -->
+      </div>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { MultiSelectPlugin, MultiSelect,CheckBoxSelection } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(MultiSelectPlugin);
+MultiSelect.Inject(CheckBoxSelection);
+export default Vue.extend({
+  data: function() {
+    return {
+      //  dataItem : [
+      //       'Aeróbics',
+      //       'Aeróbics en Agua',
+      //       'Aerografía',
+      //       'Aeromodelaje',
+      //       'Águilas',
+      //       'Ajedrez',
+      //       'Ala Delta',
+      //       'Álbumes de Música',
+      //       'Alusivos',
+      //       'Análisis de Escritura a Mano'
+      //   ],
+      dataItem: [
+        { Name: 'Cabbage', Category: 'Leafy and Salad', Id: 'item1' },
+        { Name: 'Spinach', Category: 'Leafy and Salad', Id: 'item2' },
+        { Name: 'Wheat grass', Category: 'Leafy and Salad', Id: 'item3' },
+        { Name: 'Yarrow', Category: 'Leafy and Salad', Id: 'item4' },
+        { Name: 'Pumpkins', Category: 'Leafy and Salad', Id: 'item5' },
+        { Name: 'Chickpea', Category: 'Beans', Id: 'item6' },
+        { Name: 'Green bean', Category: 'Beans', Id: 'item7' },
+        { Name: 'Horse gram', Category: 'Beans', Id: 'item8' }
+        ],
+      dataFields: { 
+        groupBy: 'Category', 
+        text: 'Name', 
+        value: 'Id' }
+     }
+  }
+});
+
+</script>
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+</style>
